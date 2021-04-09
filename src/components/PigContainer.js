21dda8a6c5ec59689acdc876c5879c67d763be68
Danny <danny.costelloe@gmail.com>
2021-04-09 13:16:14 -0400

    working on filter

--- src/components/PigContainer.js
+++ src/components/PigContainer.js
@@ -2,25 +2,36 @@ import PigList from "./PigList.js"
 import React, {useState} from "react"
 
 function PigContainer({pigs}){
-    const [isGreasy, setIsGreasy]= useState(false)
+    const [isGreasy, setIsGreasy]= useState("All")
 
-    function handleGreasyFilter(){
-        const filterPiggyArr = pigs.filter((pig) =>{
-            return(pig.greased === isGreasy)
-        })
-        setIsGreasy(!isGreasy)
-        console.log(filterPiggyArr)
-        const filterPigs = filterPiggyArr.map((pig) =>{
-            return( <PigList
-                key = {pig.name} 
-                name = {pig.name} 
-                imgSrc = {pig.image} 
-                specialty = {pig.specialty}
-                greased = {pig.greased}
-                weight = {pig.weight}
-                medal = {pig["highest medal achieved"]}/>)
-        })
-    }
+    function handleGreasyChange(event) {
+        console.log(event.target.value);
+        setIsGreasy(event.target.value)
+      }
+
+      const pigsToDisplay = pigs.filter((pig) => {
+        if (isGreasy === "All") return true;
+    
+        return pig.greased === isGreasy;
+      });
+    
+    // function handleGreasyFilter(){
+    //     const filterPiggyArr = pigs.filter((pig) =>{
+    //         return(pig.greased === isGreasy)
+    //     })
+    //     setIsGreasy(!isGreasy)
+    //     console.log(filterPiggyArr)
+    //     const filterPigs = filterPiggyArr.map((pig) =>{
+    //         return( <PigList
+    //             key = {pig.name} 
+    //             name = {pig.name} 
+    //             imgSrc = {pig.image} 
+    //             specialty = {pig.specialty}
+    //             greased = {pig.greased}
+    //             weight = {pig.weight}
+    //             medal = {pig["highest medal achieved"]}/>)
+    //     })
+    // }
     
     const pigListArray = pigs.map((pig) =>{
         return( <PigList
@@ -38,10 +49,19 @@ function PigContainer({pigs}){
 return(
     <div id = "pig-containter">
       <h1>Pigs</h1>
-      <select name="filter" >
-        <option onClick={setIsGreasy("All")}value="All">Filter by category</option>
-        <option onClick={setIsGreasy("Greased")} value="Greased">Greased</option>
-        <option onClick={setIsGreasy("Not Greased")} value="Not Greased">Not Greased</option>
+      <select name="filter" onChange={handleGreasyChange}>
+        <option value="All">Filter by category</option>
+        <option value="Greased">Greased</option>
+        <option value="Not Greased"> Not Greased {pigsToDisplay.map((pig) => 
+        <PigList
+            key = {pig.name} 
+            name = {pig.name}
+            imgSrc = {pig.image} 
+            specialty = {pig.specialty}
+            greased = {pig.greased}
+            weight = {pig.weight}
+            medal = {pig["highest medal achieved"]} /> )}
+        </option>
       </select>
       
       <ul>{pigListArray}</ul>  
