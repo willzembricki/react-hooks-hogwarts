db039a3529800c72a0a4960a472c7477123420dc
Danny <danny.costelloe@gmail.com>
2021-04-09 12:04:46 -0400

    DannyDriving

--- src/components/PigContainer.js
+++ src/components/PigContainer.js
@@ -2,7 +2,26 @@ import PigList from "./PigList.js"
 import React, {useState} from "react"
 
 function PigContainer({pigs}){
-    const [isGreasy, setIsGreasy]= useState("false")
+    const [isGreasy, setIsGreasy]= useState(false)
+
+    function handleGreasyFilter(){
+        const filterPiggyArr = pigs.filter((pig) =>{
+            return(pig.greased === isGreasy)
+        })
+        setIsGreasy(!isGreasy)
+        console.log(filterPiggyArr)
+        const filterPigs = filterPiggyArr.map((pig) =>{
+            return( <PigList
+                key = {pig.name} 
+                name = {pig.name} 
+                imgSrc = {pig.image} 
+                specialty = {pig.specialty}
+                greased = {pig.greased}
+                weight = {pig.weight}
+                medal = {pig["highest medal achieved"]}/>)
+        })
+    }
+    
     const pigListArray = pigs.map((pig) =>{
         return( <PigList
             key = {pig.name} 
@@ -19,7 +38,11 @@ function PigContainer({pigs}){
 return(
     <div id = "pig-containter">
       <h1>Pigs</h1>
-      <button>Greased</button>
+      <select name="filter" >
+        <option onClick={setIsGreasy("All")}value="All">Filter by category</option>
+        <option onClick={setIsGreasy("Greased")} value="Greased">Greased</option>
+        <option onClick={setIsGreasy("Not Greased")} value="Not Greased">Not Greased</option>
+      </select>
       
       <ul>{pigListArray}</ul>  
         
